df4f021708d2291d64d4d21a725fd15447ad60ba
Andrei Genisev <a.genisev@syberry.com>
2024-05-06 11:20:21 +0300

    Add menu and "Project" page

--- UI/WorkChronicle/WorkChronicle/Controllers/BaseController.cs
+++ UI/WorkChronicle/WorkChronicle/Controllers/BaseController.cs
@@ -4,16 +4,21 @@
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
 
+    using Utilities.Mapper;
+
     using WebApiClients;
 
     [Authorize]
     public class BaseController : Controller
     {
-        protected readonly WebApiClient webApiClient;
+        protected readonly WebApiClient WebApiClient;
+
+        protected readonly IMapper Mapper;
 
         public BaseController(IConfiguration configuration)
         {
-            webApiClient = new WebApiClient(configuration.GetValue<string>("BaseApiUrl"));
+            WebApiClient = new WebApiClient(configuration.GetValue<string>("BaseApiUrl"));
+            Mapper = new Mapper();
         }
     }
 }
